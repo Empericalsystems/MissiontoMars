59970c41870e76694dfd2909330e22442224a946
N. Adams <you@example.com>
2021-08-22 12:27:01 +0000

    change js folder

--- server.py
+++ server.py
@@ -9,8 +9,7 @@ import os
 import requests
 
 
-
-app = Flask(__name__)
+app = Flask (__name__)
 app.secret_key = 'nosecret'
 
 
@@ -20,8 +19,6 @@ def homepage():
     return render_template('homepage.html')
 
     
-    
-
 
 @app.route('/curiosity')
 def log_Curiosity():
@@ -45,34 +42,7 @@ def log_opportunity():
 
     return render_template('opportunity')
 
-# @app.route('/afterparty/search')
-# def find_afterparties():
-#     """Search for afterparties on Eventbrite"""
-
-#     keyword = request.args.get('keyword', '')
-#     postalcode = request.args.get('zipcode', '')
-#     radius = request.args.get('radius', '')
-#     unit = request.args.get('unit', '')
-#     sort = request.args.get('sort', '')
-
-#     url = 'https://app.ticketmaster.com/discovery/v2/events'
-#     payload = {'apikey': API_KEY,
-#                'keyword': keyword,
-#                'postalCode': postalcode,
-#                'radius': radius,
-#                'unit': unit,
-#                'sort': sort}
-
-#     response = requests.get(url, params=payload)
-
-#     data = response.json()
-#     events = data['_embedded']['events']
-
-#     return render_template('search-results.html',
-#                            pformat=pformat,
-#                            data=data,
-#                            results=events)
-
+ 
 
 if __name__ == '__main__':
     # app.debug = True

e4fcdc5a4082bf5bbfb89531afc28c5bf2090cd4
N. Adams <you@example.com>
2021-09-17 11:24:46 +0000

    Styling

---
+++ static/play_sound.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let playAudio = () => new Audio("/static/sounds/mars_sound.mp3").play
+
+// $.get("#marsSounds").controls = True;
+
+// const marsRSound = document.getElementById("marsSound");
+// function playAudio() {
+//     marsRSound.play();
+// }
+// function pauseAudio (){
+//     marsRSound.pause();
+// }
+
+
+// $('#sound').on('click', (evt) => {
+//     evt.preventDefault();
+    
+//     const marsSound = {
+//         play: audio("/static/sounds/mars_sound.mp3")
+
+//     }
+    
+    
+
+
+// });
+
+// $('#form_search').on('submit', (evt) =>{
+//     evt.preventDefault();
+
+//     const formQuery = {
+//         'search_title': $('#search_title').val()
+      
+//     };
+
+//     $.post('/search', formQuery, (res) =>{
+    
+//         res.json().forEach((item) =>{
+//             console.log(item);
+//             $('#blog').append(item);
+//         });
+//     });
+// });
+
+  
+ 
